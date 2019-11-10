3afb41810ba7a3bb88c99e5695c06dad16dbb8af
Martin <32516410+Xerenz@users.noreply.github.com>
2019-11-10 11:00:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAFEIc_Po4HPQ7mOg1SSL_lg56mJw0qAcE",
-    authDomain: "campus-ambassador-b3c7a.firebaseapp.com",
-    databaseURL: "https://campus-ambassador-b3c7a.firebaseio.com",
-    projectId: "campus-ambassador-b3c7a",
-    storageBucket: "campus-ambassador-b3c7a.appspot.com",
-    messagingSenderId: "714049994844",
-    appId: "1:714049994844:web:6a8c6363605c9b4bfc2699",
-    measurementId: "G-6JS999HH5P"
-};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
